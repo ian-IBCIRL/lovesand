3bbe974ec252d70e755d77ebe95c8778cd9389fb
ian-IBCIRL <ian@ibcirl.com>
2022-10-04 14:31:15 +0000

    added data input and display check

--- run.py
+++ run.py
@@ -30,3 +30,18 @@ surplusdata = surplussheet.get_all_values()
 print(data)
 print("--------------------")
 print(surplusdata)
+
+
+def get_sales_data():
+    """
+    Get sales figures input from the user.
+    """
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, separated by commas.")
+    print("Example: 10,20,30,40,50,60\n")
+
+    data_str = input("Enter your data here: ")
+    print(f"The data provided is {data_str}")
+
+
+get_sales_data()
